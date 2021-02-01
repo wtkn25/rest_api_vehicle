3acde5c16e1f789c54e978a55aac237d4619bfce
wtkn25 <atrastar72@gmail.com>
2021-02-01 15:09:55 +0900

    モデル作成

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Segment, Brand, Vehicle
 
-# Register your models here.
+admin.site.register(Segment)
+admin.site.register(Brand)
+admin.site.register(Vehicle)
